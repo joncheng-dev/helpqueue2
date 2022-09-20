86730dc056ecacc74b49775bcd4492c707cb2d3b
Jonathan Cheng <jcheng461@gmail.com>
2022-09-20 19:35:31 +0900

    update ticket detail page to correctly display selected ticket's information. Fix button to return to ticket list page

--- src/components/TicketDetail.js
+++ src/components/TicketDetail.js
@@ -1,12 +1,25 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function TicketDetail(props) {
+  const { ticket } = props;
+
   return (
     <React.Fragment>
-      <h3>Ticket Detail</h3>
+      <hr />
+      <h1>Ticket Detail</h1>
+      <h3>{ticket.location}</h3>
+      <h3>{ticket.names}</h3>
+      <p>
+        <em>{ticket.issue}</em>
+      </p>
       <hr />
     </React.Fragment>
   );
 }
+
+TicketDetail.propTypes = {
+  ticket: PropTypes.object,
+};
 
 export default TicketDetail;
