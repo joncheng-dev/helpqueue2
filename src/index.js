77cf9a49b0093762d5c359541878156e80d16718
Jonathan Cheng <jcheng461@gmail.com>
2022-12-29 18:03:14 +0900

    refactor application to use root reducer

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
 import { createStore } from "redux";
-import reducer from "./reducers/ticket-list-reducer";
+import rootReducer from "./reducers/index";
 import { Provider } from "react-redux";
 
-const store = createStore(reducer);
+const store = createStore(rootReducer);
 
 store.subscribe(() => console.log(store.getState()));
 
